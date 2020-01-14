874163912529ea1479feb6248c0b1f258a988fe4
ryankayne <coope1rk@gmail.com>
2020-01-14 18:10:56 -0500

    Almost done

--- api/routers/postsRouter.js
+++ api/routers/postsRouter.js
@@ -65,15 +65,26 @@ router.post('/', (req, res) => {
     }
 });
 
-// router.post('/:id', (req, res) => {
-//     Database.find(req.query)
-//     .then(db => {
-//         res.status(200).json(db);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//         res.status(500).json({ message: 'Error retrieving post'});
-//     });
-// });
+router.post('/:id/comments', (req, res) => {
+    const { text, post_id } = req.body;
+    const post_id = req.params.id;
+
+    if (!post_id) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+    } else if (!text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." });
+    } else {
+        Database.insertComment({ text, post_id })
+        .then(newComment => {
+            res.status(201).json(newComment);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "There was an error while saving the comment to the database" });
+        });
+    }
+});
+
+
 
 module.exports = router;
