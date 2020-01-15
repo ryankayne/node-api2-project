a9fe658e87b7e3f956a8077365966f1231fa5a1e
ryankayne <coope1rk@gmail.com>
2020-01-15 16:31:04 -0500

    PUT changes

--- api/routers/postsRouter.js
+++ api/routers/postsRouter.js
@@ -109,10 +109,11 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.put('/api/posts/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const changes = req.body;
-  
-  Database.update(req.params.id, changes)
+  const id = req.params.id;
+
+  Database.update(id, changes)
   .then(post => {
     if (post) {
       res.status(200).json(post);
@@ -126,5 +127,30 @@ router.put('/api/posts/:id', (req, res) => {
     res.status(500).json({ error: "The post information could not be modified." });
   });
 });
+
+// router.put('/:id/comments', (req, res) => {
+//     const { text } = req.body;
+//     const post_id = req.params.id;
+
+//     Database.findById(req.params.id)
+//     .then(post => {
+//         if (!post[0]) {
+
+//         res.status(404).json({ message: "The post with the specified ID does not exist." });
+//     } else {
+//         if (!req.body.text) {
+//         res.status(400).json({ errorMessage: "Please provide text for the comment." });
+//     } else {
+//         Database.insertComment({ text, post_id })
+//         .then(newComment => {
+//             res.status(201).json(newComment);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             res.status(500).json({ error: "There was an error while saving the comment to the database" });
+//         });
+//     }}
+// });
+// });
 
 module.exports = router;
